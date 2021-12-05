53205ab0318e3d21668d1a456e3f285461bc64ef
Adel <shi.samadel@gmail.com>
2021-12-05 11:08:49 +0300

    refactor nms

--- core.py
+++ core.py
@@ -12,44 +12,24 @@ def calc_grad_img(img):
     return grad_x, grad_y
 
 
-def non_maximum_suppression2(magnitude_img, angles):
+def non_maximum_suppression(magnitude_img, angles):
     clear_img = np.zeros_like(magnitude_img)
     cropped_angles = angles[1:-1, 1:-1]
 
-    lines_0_180 = np.where((cropped_angles == 0) | (cropped_angles == 180))
-    lines_0_180_y = lines_0_180[0] + 1
-    lines_0_180_x = lines_0_180[1] + 1
+    directions = {0:{'angles':(0, 180), 'pixels':[(0,-1), (0, 1)]},
+                  1:{'angles':(45, -45), 'pixels':[(-1,1), (1, -1)]},
+                  2:{'angles':(90, -90), 'pixels':[(1,0), (-1, 0)]},
+                  3:{'angles':(135, -135), 'pixels':[(-1,-1), (1, 1)]}}
 
-    temp = np.maximum(magnitude_img[lines_0_180_y, lines_0_180_x - 1], magnitude_img[lines_0_180_y, lines_0_180_x + 1])
-    idxs_change_vals = np.where(magnitude_img[lines_0_180_y, lines_0_180_x] >= temp)
-    clear_img[lines_0_180_y[idxs_change_vals[0]], lines_0_180_x[idxs_change_vals[0]]] = temp[idxs_change_vals[0]]
+    for dir in directions.values():
+        lines_0_180 = np.where((cropped_angles == dir['angles'][0]) | (cropped_angles == dir['angles'][1]))
+        lines_0_180_y = lines_0_180[0] + 1
+        lines_0_180_x = lines_0_180[1] + 1
 
-    lines_45_m45 = np.where((cropped_angles == 45) | (cropped_angles == -45))
-    lines_45_m45_y = lines_45_m45[0] + 1
-    lines_45_m45_x = lines_45_m45[1] + 1
-
-    temp = np.maximum(magnitude_img[lines_45_m45_y - 1, lines_45_m45_x + 1],
-                      magnitude_img[lines_45_m45_y + 1, lines_45_m45_x - 1])
-    idxs_change_vals = np.where(magnitude_img[lines_45_m45_y, lines_45_m45_x] >= temp)
-    clear_img[lines_45_m45_y[idxs_change_vals[0]], lines_45_m45_x[idxs_change_vals[0]]] = temp[idxs_change_vals[0]]
-
-    lines_90_m90 = np.where((cropped_angles == 90) | (cropped_angles == -90))
-    lines_90_m90_y = lines_90_m90[0] + 1
-    lines_90_m90_x = lines_90_m90[1] + 1
-
-    temp = np.maximum(magnitude_img[lines_90_m90_y + 1, lines_90_m90_x],
-                      magnitude_img[lines_90_m90_y - 1, lines_90_m90_x])
-    idxs_change_vals = np.where(magnitude_img[lines_90_m90_y, lines_90_m90_x] >= temp)
-    clear_img[lines_90_m90_y[idxs_change_vals[0]], lines_90_m90_x[idxs_change_vals[0]]] = temp[idxs_change_vals[0]]
-
-    lines_135_m135 = np.where((cropped_angles == 135) | (cropped_angles == -135))
-    lines_135_m135_y = lines_135_m135[0] + 1
-    lines_135_m135_x = lines_135_m135[1] + 1
-
-    temp = np.maximum(magnitude_img[lines_135_m135_y - 1, lines_135_m135_x - 1],
-                      magnitude_img[lines_135_m135_y + 1, lines_135_m135_x + 1])
-    idxs_change_vals = np.where(magnitude_img[lines_135_m135_y, lines_135_m135_x] >= temp)
-    clear_img[lines_135_m135_y[idxs_change_vals[0]], lines_135_m135_x[idxs_change_vals[0]]] = temp[idxs_change_vals[0]]
+        temp = np.maximum(magnitude_img[lines_0_180_y + dir['pixels'][0][0], lines_0_180_x + dir['pixels'][0][1]],
+                          magnitude_img[lines_0_180_y + dir['pixels'][1][0], lines_0_180_x + dir['pixels'][1][1]])
+        idxs_change_vals = np.where(magnitude_img[lines_0_180_y, lines_0_180_x] >= temp)[0]
+        clear_img[lines_0_180_y[idxs_change_vals], lines_0_180_x[idxs_change_vals]] = temp[idxs_change_vals]
 
     return clear_img
     
