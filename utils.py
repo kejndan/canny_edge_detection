71f702fd2a3e1d971c8f0e91ead16849c7d3f3cc
Adel <shi.samadel@gmail.com>
2021-12-05 11:59:52 +0300

    change utils

--- utils.py
+++ utils.py
@@ -1,20 +1,15 @@
-import cv2
+from cv2 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-def read_image(path, color=False):
-    if color:
-        image = np.array(cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB), dtype=np.float64)
-    else:
-        image = np.array(cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY), dtype=np.float64)
-    return image
 
-def show_image(image, title=None, cmap=None, textbox=None):
-    if cmap is not None:
-        plt.imshow(np.uint8(image),cmap=cmap)
-    else:
-        plt.imshow(np.uint8(image))
-    if title is not None:
-        plt.title(title)
+def read_image(path):
+    rgb_image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
+    return rgb_image
 
+def show_image(img):
+    plt.imshow(img, cmap='gray')
     plt.axis('off')
     plt.show()
+
+def save_image(img, name):
+    cv2.imwrite(name, img)
